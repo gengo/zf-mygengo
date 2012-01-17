1a0269869e3d6132b81e9c72e6c5b525c6acc9e9
Lloyd <lloyd.chan@mygengo.com>
2012-01-17 17:48:33 +0900

    Change user-agent to reflect best practices http://mygengo.com/api/developer-docs/best-practices/

--- library/myGengo/Client.php
+++ library/myGengo/Client.php
@@ -28,7 +28,7 @@ class myGengo_Client
     {
         $this->config = myGengo_Config::getInstance();
         $config = array('maxredirects' => 5,
-                        'useragent' => 'phpMyGengo 1.0',
+                        'useragent' => 'myGengo ZF-Mygengo; Version 1.0; https://github.com/shell/zf-mygengo;',
                         'timeout' => 10,
                         'keepalive' => false);
         $this->client = new Zend_Http_Client(null, $config);
